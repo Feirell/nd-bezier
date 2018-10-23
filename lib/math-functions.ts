a84241be650f550f024ea76b7d205bcce9583dbf
Florian Völker <Neirolf2030@web.de>
2018-10-24 00:16:43 +0200

    fixed multiple issues with names

--- lib/math-functions.ts
+++ lib/math-functions.ts
@@ -1,7 +1,4 @@
-declare interface Point {
-    x: number,
-    y: number
-}
+import { Point } from './point';
 
 const factorial = (function () {
     const factorialCache = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000];
--- lib/produced-specific-bezier.ts
+++ lib/produced-specific-bezier.ts
@@ -1,4 +1,5 @@
-import { Point } from './Point';
+import { Point } from './point';
+
 import {
     bc
 } from './math-functions';
