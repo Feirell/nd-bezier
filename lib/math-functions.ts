6683c1df375b87536acedcc3623d23472c65d78b
Völker, Florian <Florian.Voelker@adesso.de>
2020-09-28 16:44:45 +0200

    cleaned math-function

--- lib/math-functions.ts
+++ lib/math-functions.ts
@@ -1,9 +1,9 @@
-const factorial = (function () {
+export const factorial = (function () {
     const factorialCache = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000];
 
     /**
-     * Calculates n! 
-     * @param n 
+     * Calculates n!
+     * @param n
      */
     function factorial(n: number) {
         return factorialCache[n];
@@ -12,26 +12,13 @@ const factorial = (function () {
     return factorial;
 })();
 
-/**
- * Calculates the k which is the closest n still fitting in n! so n! <= k
- * @param k 
- */
-function reverseFactorial(k: number) {
-    let f = 1;
-
-    while (true)
-        if (factorial(++f) >= k)
-            return f - 1; // lower bound (f! which will fit in the given n, for upper bound just return f)
-
-}
-
-const bc = (function () {
+export const bc = (function () {
     const bcCache: number[][] = [];
 
     /**
      * Calculates the binomial coefficient for n over k
-     * @param n 
-     * @param k 
+     * @param n
+     * @param k
      */
     function bc(n: number, k: number): number {
 
@@ -47,7 +34,7 @@ const bc = (function () {
         return bcCache[n][k];
     }
 
-    // building up cache 
+    // building up cache
     for (let n = 0; n <= 15; n++) {
         bcCache[n] = [];
         for (let k = 0; k <= Math.ceil(n / 2); k++) {
@@ -58,86 +45,3 @@ const bc = (function () {
     return bc;
 })();
 
-/**
- * Calculates the sum for n (e.g. n = 5;sum(5) = 5+4+3+2+1 )
- * 
- * @param n 
- */
-function sum(n: number) {
-    return n * (n + 1) / 2
-}
-
-/**
- * The opposite calculation of sum(n)
- * 
- * @param k 
- */
-function reverseSum(k: number) {
-    return Math.sqrt(2 * k + 0.25) - 0.5
-}
-
-/**
- * Shorthand for Math.pow
- * @param n 
- * @param k 
- */
-const pow = (n: number, k: number) => Math.pow(n, k);
-
-/**
- * Calculates the level of the given i, see the example for more information.
- * @param i 
- */
-function lvl(i: number) {
-    return Math.floor(reverseSum(i));
-};
-
-/**
- * Calculates the level of the given intermediate index of the given bezier grade.
- * 
- * Example:
- * 
- * i = 3, grade = 3
- * ```text
- *          level
- *  0 1 2   2
- *   3 4    1       <= the level of the given index 3
- *    5     0
- * ```
- * 
- * @param bezierGrade 
- * @param i 
- */
-function levelReverse(bezierGrade: number, i: number) {
-    return lvl(sum(bezierGrade + 1) - i - 1);
-};
-
-/**
- * Calculates the level of the given intermediate index of the given bezier grade.
- * 
- * Example:
- * 
- * i = 3, grade = 3
- * ```text
- *          level
- *  0 1 2   0
- *   3 4    1       <= the level of the given index 3
- *    5     2
- * ```
- * 
- * @param bezierGrade 
- * @param i 
- */
-function level(bezierGrade: number, i: number) {
-    return lvl(sum(bezierGrade + 1)) - lvl(sum(bezierGrade + 1) - i - 1) - 1;
-};
-
-export {
-    bc,
-    factorial,
-    reverseFactorial,
-    sum,
-    reverseSum,
-    pow,
-    level,
-    levelReverse
-};
